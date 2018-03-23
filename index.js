f50bdc76e27aaa6d84dafe4606697d4f1cd87e1b
zw-zack <zhengwin94@hotmail.com>
2018-03-23 10:26:42 +0800

    completed without bonus

---
+++ index.js
@@ -0,0 +1,47 @@
+let preCodeWord = ""
+let postCodeWord = ""
+
+for(var b = 2; b < process.argv.length; b++){
+	let inputWord = process.argv[b];
+
+	for(var i = 0; i < inputWord.length ; i++){
+		preCodeWord += inputWord.charAt(i).toUpperCase() + " "
+
+		for (var a = 0; a < 25; a++){
+			let codes = {
+				"A" : "aaaaa", 
+				"B" : "aaaab",
+				"C" : "aaaba",    
+				"D" : "aaabb",
+				"E" : "aabaa",    
+				"F" : "aabab",    
+				"G" : "aabba",
+				"H" : "aabbb", 
+				"I" : "abaaa",
+				"J" : "bbbaa",
+				"K" : "abaab",
+				"L" : "ababa",
+				"M" : "ababb",
+				"N" : "abbaa",
+				"O" : "abbab",
+				"P" : "abbba",
+				"Q" : "abbbb",
+				"R" : "baaaa",
+				"S" : "baaab",
+				"T" : "baaba",
+				"U" : "baabb",
+				"V" : "bbaab",
+				"W" : "babaa",
+				"X" : "babab",
+				"Y" : "babba",
+				"Z" : "babbb"
+			}
+
+			if(inputWord.charAt(i).toUpperCase() === Object.keys(codes)[a]) {
+				postCodeWord += codes[inputWord.charAt(i).toUpperCase()] + " "
+			}
+		}
+	}
+}
+console.log(preCodeWord);
+console.log(postCodeWord);
